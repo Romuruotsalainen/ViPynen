572f5e620cb4604600accceee5676467ecc20937
Markus Söderman <markus.soderman@gmail.com>
2020-04-26 14:07:37 +0300

    Add files via upload

---
+++ ViPynen.py
@@ -0,0 +1,14 @@
+import urllib.request
+import requests
+import json
+
+
+
+main():
+    while True:
+        kommando = input("Språk/Kieli/Language: sv, fi, en")
+        #TODO: Next step will be according to language choice
+        if kommando = "sv":
+            kommando = input("Skriv in kommando eller skriv kommandon \n för att se möjliga alternativ. Skriv sluta för att avlutsa")
+            if kommando = "avsluta":
+                break
